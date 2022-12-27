67431ea159437a021d46bef445983c8dfdc61b78
Bailee <bailee.roby@gmail.com>
2022-12-26 16:49:05 -0800

    changing my site look

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function github() {
+    var link = document.createElement("a")
+    link.href = "https://www.github.com/thebaiway"
+    link.target = "_blank"
+    link.click()
+  }
+
+  function instagram() {
+    var link = document.createElement("a")
+    link.href = "https://www.instagram.com/thebaiway"
+    link.target = "_blank"
+    link.click()
+  }

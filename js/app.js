74e0ebc266c93972035241d6e558c476f5cd4702
Bailee <bailee.roby@gmail.com>
2023-01-08 18:29:40 -0800

    updating app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,53 @@
-function github() {
-    var link = document.createElement("a")
-    link.href = "https://www.github.com/thebaiway"
-    link.target = "_blank"
-    link.click()
-  }
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-  function instagram() {
-    var link = document.createElement("a")
-    link.href = "https://www.instagram.com/thebaiway"
-    link.target = "_blank"
-    link.click()
-  }
+function Component () {
+    return (
+    <div>
+        <Header /> 
+        <MainContent />
+        <Footer />
+        </div>
+    )
+}
 
-  function twitter() {
-    var link = document.createElement("a")
-    link.href = "https://www.twitter.com/thebaiway"
-    link.target = "_blank"
-    link.click()
-  }
+function Header () {
+    return (
+        <nav>
+            <div className="navbar">
+            <ul className="list-items">
+                <li> GitHub </li>
+                <li> Instagram </li>
+                <li> Twitter </li>
+            </ul>
+            </div>
+        </nav>
+    )
+}
+
+function MainContent () {
+    return (
+        <div  className="main-div">
+            <div>
+                <h1>Bailee R. </h1>
+                <p>Frontend Developer</p>
+                <p>Lacey, Washington</p>
+            </div>
+            <div>
+                <img src="img/IMG_1114.JPG" className="main-pic"></img>
+            </div>
+        </div>
+    )
+}
+
+function Footer () {
+    return (
+        <div className="footer-div">
+            <i class="fa-brands fa-github"></i>
+            <i><i class="fa-brands fa-instagram"></i></i>
+            <i><i class="fa-brands fa-twitter"></i></i>
+        </div>
+    )
+}
+
+
+root.render(<Component />)
